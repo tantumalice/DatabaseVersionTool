65b5e13693fa7209925722a5b24bf4aa58e71366
alisa.semenova <alisa.semenova@nordclan.com>
2023-04-10 17:24:40 +0300

    Main logic and config

--- DatabaseVersionTool/Program.cs
+++ DatabaseVersionTool/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace DatabaseVersionTool
+﻿using System.Configuration;
+
+namespace DatabaseVersionTool
 {
     internal class Program
     {
+        private const string confirmation = "yes";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ConnectionStringSettings connectionString = ConfigurationManager.ConnectionStrings["Main"];
+            if (string.IsNullOrEmpty(connectionString.ConnectionString))
+            {
+                Console.WriteLine("There is no connection string with the \"Main\" key");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Type \"yes\" to execute updates");
+            if (!string.Equals(Console.ReadLine(), confirmation))
+            {
+                return;
+            }
+
+            var output = new VersionManager(connectionString.ConnectionString).ExecuteMigrations();
+
+            foreach (var line in output)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
---
+++ DatabaseVersionTool/VersionManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseVersionTool;
+
+internal class VersionManager
+{
+    private readonly string _connectionString;
+
+    public VersionManager(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public IReadOnlyCollection<string> ExecuteMigrations()
+    {
+        // Get current DB version or generate one
+
+        // Find new migrations
+
+        // Evecute new migrations
+    }
+}
